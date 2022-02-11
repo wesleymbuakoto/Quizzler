fc9eb495612fcd5f956bc2f9eefaaafb81dd3e05
wesleymbuakoto <40523361+wesleymbuakoto@users.noreply.github.com>
2022-02-11 00:07:23 -0500

    Add files via upload

---
+++ android/app/src/main/kotlin/mufasa/quizzler_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package mufasa.quizzler_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/questions.dart
@@ -0,0 +1,14 @@
+class Question {
+
+  String questionText;
+  bool questionAnswer;
+
+  Question({String q, bool a}) {
+
+    questionText = q;
+    questionAnswer = a;
+
+
+  }
+
+}
